dac661e4dc4abdf96a7cd3519059726e791e9ca5
Pawel Walczewski <pwalczewski@gmail.com>
2023-07-20 14:26:25 -0400

    Save progress: 1.1

--- salon.sh
+++ salon.sh
@@ -3,35 +3,51 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 MAIN_MENU() {
+  echo -e "\n~~~~~ MY SALON ~~~~~\n"
+  if [[ $1 ]]
+  then
+  echo $1
+  fi
   SERVICES=$($PSQL "select * from services")
   echo "$SERVICES" | while read SERVICE_ID BAR NAME
   do
-    echo -e "$SERVICE_ID) $NAME"
-
+    echo "$SERVICE_ID) $NAME"
   done
   read SERVICE_ID_SELECTED
-  SERVICE_ID=$($PSQL "select service_id from services where service_id = $SERVICE_ID_SELECTED")
-  if [[ -z $SERVICE_ID ]]
+    if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
   then
     MAIN_MENU "Invalid input or unknown case."
   else
-    GET_CUSTOMER_DATA $SERVICE_ID
+  SERVICE_NAME=$($PSQL "select name from services where service_id = $SERVICE_ID_SELECTED")
+  if [[ -z $SERVICE_NAME ]]
+  then
+    MAIN_MENU "Invalid input or unknown case."
+  else
+   GET_CUSTOMER_DATA $SERVICE_ID_SELECTED "$SERVICE_NAME"
+   # echo "co sie dzieje"
+  fi
   fi
 }
 GET_CUSTOMER_DATA() {
-  read -p "Give me your phone number:" CUSTOMER_PHONE
+  echo "Give me your phone number: "
+  read CUSTOMER_PHONE
   CUSTOMER_DATA=$($PSQL "select customer_id, name from customers where phone = '$CUSTOMER_PHONE'")
   if [[ -z $CUSTOMER_DATA ]]
   then
-    read -p "Give me your name:" CUSTOMER_NAME
-    echo $($PSQL "insert into customers(name, phone) values ('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+    echo "Give me your name: "
+    read CUSTOMER_NAME
+    RESULT=$($PSQL "insert into customers(name, phone) values ('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+    echo "customer: $RESULT"
   fi
   CUSTOMER_DATA=$($PSQL "select customer_id, name from customers where phone = '$CUSTOMER_PHONE'")
-  read -p "Give me the time:" SERVICE_TIME
+  echo "Give me the time: "
+  read SERVICE_TIME
   
-  echo "$CUSTOMER_DATA" 
-  #| read CUSTOMER_ID BAR CUSTOMER_NAME
-  echo $CUSTOMER_NAME
-  #echo $($PSQL "insert into appointments(customer_id, service_id, time" values($CUSTOMER_ID, $1, '$SERVICE_TIME'))
+  read CUSTOMER_ID BAR CUSTOMER_NAME <<< "$CUSTOMER_DATA"
+  RESULT2=$($PSQL "insert into appointments(customer_id, service_id, time) values($CUSTOMER_ID, $1, '$SERVICE_TIME')")
+  if [[ $RESULT2 == "INSERT 0 1" ]]
+  then
+   echo "I have put you down for a$2 at $SERVICE_TIME, $CUSTOMER_NAME."
+  fi
 }
 MAIN_MENU
